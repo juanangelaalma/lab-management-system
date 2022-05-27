7790f7f3d5d188247dbee6e567041927baafb8fe
juanangelaalma <juanlah77@gmail.com>
2022-05-27 08:48:04 +0700

    change relation loans_inventory_id_foreign to ON DELETE=NULL

---
+++ database/migrations/2022_05_27_003834_change_inventory_id_in_loans_to_ondelete_null.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeInventoryIdInLoansToOndeleteNull extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->foreign('inventory_id')->references('id')->on('inventories')->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('loans', function (Blueprint $table) {
+            $table->foreign('inventory_id')->references('id')->on('inventories')->onDelete('cascade');
+        });
+    }
+}
